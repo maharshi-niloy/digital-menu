ddbbdc83b458acfdbeb9a10b998d05129f35c343
Maharshi Niloy <82697213+maharshi-niloy@users.noreply.github.com>
2023-06-16 00:02:42 +0600

    App.js

--- src/App.js
+++ src/App.js
@@ -45,61 +45,60 @@ export default function App() {
     <ThemeProvider theme={darkTheme}>
         <CssBaseline />
         <div className="App">
-            <Grid container spacing={1}>
-                <Navbar handleFilter={handleFilter}
-                check={darkMode} change={()=>setDarkMode(!darkMode)}
-                handleAnchorElCat={handleAnchorElCat} anchorElCat={anchorElCat}
-                handleAnchorElDie={handleAnchorElDie} anchorElDie={anchorElDie}
-                handleSearch={handleSearch} search={search}
-                />
+            <Navbar handleFilter={handleFilter}
+            check={darkMode} change={()=>setDarkMode(!darkMode)}
+            handleAnchorElCat={handleAnchorElCat} anchorElCat={anchorElCat}
+            handleAnchorElDie={handleAnchorElDie} anchorElDie={anchorElDie}
+            handleSearch={handleSearch} search={search}
+            />
 
-                <Alerts notifyCat={notifyCat} notifyDie={notifyDie} noFilter={noFilter}
-                handleNotifyCat={()=>setNotifyCat(false)}
-                handleNotifyDie={()=>setNotifyDie(false)}
-                handleNoFilter={()=>setNoFilter(false)}
-                />
-                
-        {Data&& Data.filter((item) => {
-            if (filter === 'Appetizers'){
-                if (search !== ''){
-                    return item.name.toLowerCase().includes(search.toLowerCase());
-                }else
-                return item.category.includes("Appetizers");
-            }else if(filter === 'Main Courses'){ 
-                if (search !== ''){
-                    return item.name.toLowerCase().includes(search.toLowerCase());
-                }else
-                return item.category.includes("Main Courses");
-            }else if(filter === 'Desserts'){
-                if (search !== ''){
-                    return item.name.toLowerCase().includes(search.toLowerCase());
-                }else 
-                return item.category.includes("Desserts");
-            }else if(filter === 'Beverages'){
-                if (search !== ''){
-                    return item.name.toLowerCase().includes(search.toLowerCase());
-                }else
-                return item.category.includes("Beverages");
-            }else if (filter === 'Vegan'){
-                if (search !== ''){
-                    return item.name.toLowerCase().includes(search.toLowerCase());
-                }else
-                return item.dietary.includes("Vegan");
-            }else if (filter === 'Gluten-Free'){
-                if (search !== ''){
-                    return item.name.toLowerCase().includes(search.toLowerCase());
-                }else
-                return item.dietary.includes("Gluten-Free");
-            }else if (filter === 'Dairy-Free'){
-                if (search !== ''){
-                    return item.name.toLowerCase().includes(search.toLowerCase());
-                }else{
-                    return item.dietary.includes("Dairy-Free");
-                }
-            }else if (search !== ''){
-                return item.name.toLowerCase().includes(search.toLowerCase());
-            }else return item;
-        }).map((item,index)=><Grid item lg={3} md={3} sm={4} xs={6}><Cards key={index} name={item.name} desc={item.desc} price={item.price} img={item.img} category={item.category} dietary={item.dietary} ingred={item.ingred} prepTime={item.prepTime} /></Grid>)}
+            <Alerts notifyCat={notifyCat} notifyDie={notifyDie} noFilter={noFilter}
+            handleNotifyCat={()=>setNotifyCat(false)}
+            handleNotifyDie={()=>setNotifyDie(false)}
+            handleNoFilter={()=>setNoFilter(false)}
+            />
+            <Grid container spacing={1}>
+                {Data&& Data.filter((item) => {
+                    if (filter === 'Appetizers'){
+                        if (search !== ''){
+                            return item.name.toLowerCase().includes(search.toLowerCase());
+                        }else
+                        return item.category.includes("Appetizers");
+                    }else if(filter === 'Main Courses'){ 
+                        if (search !== ''){
+                            return item.name.toLowerCase().includes(search.toLowerCase());
+                        }else
+                        return item.category.includes("Main Courses");
+                    }else if(filter === 'Desserts'){
+                        if (search !== ''){
+                            return item.name.toLowerCase().includes(search.toLowerCase());
+                        }else 
+                        return item.category.includes("Desserts");
+                    }else if(filter === 'Beverages'){
+                        if (search !== ''){
+                            return item.name.toLowerCase().includes(search.toLowerCase());
+                        }else
+                        return item.category.includes("Beverages");
+                    }else if (filter === 'Vegan'){
+                        if (search !== ''){
+                            return item.name.toLowerCase().includes(search.toLowerCase());
+                        }else
+                        return item.dietary.includes("Vegan");
+                    }else if (filter === 'Gluten-Free'){
+                        if (search !== ''){
+                            return item.name.toLowerCase().includes(search.toLowerCase());
+                        }else
+                        return item.dietary.includes("Gluten-Free");
+                    }else if (filter === 'Dairy-Free'){
+                        if (search !== ''){
+                            return item.name.toLowerCase().includes(search.toLowerCase());
+                        }else{
+                            return item.dietary.includes("Dairy-Free");
+                        }
+                    }else if (search !== ''){
+                        return item.name.toLowerCase().includes(search.toLowerCase());
+                    }else return item;
+                }).map((item,index)=><Grid item lg={3} md={3} sm={4} xs={6}><Cards key={index} name={item.name} desc={item.desc} price={item.price} img={item.img} category={item.category} dietary={item.dietary} ingred={item.ingred} prepTime={item.prepTime} /></Grid>)}
             </Grid>
         </div>
     </ThemeProvider>
